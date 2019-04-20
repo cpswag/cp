64dc6fbc0454d7d229b87fc59bc8f7eae9942221
Barun Singhania <emailbarung@gmail.com>
2019-04-20 19:46:25 +0530

    Stockspan prob

---
+++ random/stock-span.cpp
@@ -0,0 +1,107 @@
+/* Problem Description:
+
+The span si of a stock’s price on a certain day i is the minimum number of consecutive days (up to the current day) the price of the stock has been less than its price on that ith day. If for a particular day, if no stock price is greater then just count the number of days till 0th day including current day.
+For eg. if given price array is {3, 6, 8, 1, 2}, span for 4th day (which has price 2) is 2 because minimum count of consecutive days (including 4th day itself) from current day which has price less than 4th day is 2 (i.e. day 3 & 4). Similarly, span for 2nd day is 3 because no stock price in left is greater than 2nd day's price. So count is 3 till 0th day.
+Given an input array with all stock prices, you need to return the array with corresponding spans of each day.
+Note : Try doing it in O(n).
+Input format :
+Line 1 : Integer n, Arrays Size
+Line 2 : Price for n days (separated by space). It can contain duplicate values`
+Output format :
+Return an array that contain span for each day
+Sample Input 1 :
+8
+60 70 80 100 90 75 80 120
+Sample Output 1 :
+1 2 3 4 1 1 2 8
+Sample Input 2 :
+ 4
+ 1 1 1 1
+Sample Output 2 :
+1 1 1 1
+
+
+*/
+
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+
+#define REP(i,a,b) for (int i = (a); i < (b); i++)
+#define RREP(i,a,b) for (int i = (a); i > (b); i--)
+#define FOR(i,a,b) for (int i = (a); i <= (b); i++)
+#define TRvi(it, c) for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define inf 0x3f3f3f3f
+#define EPS 1e-10
+#define PI acos(-1.0)
+#define scan(x) scanf("%d",x)
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define LSOne(x) x&(-x)
+#define debug(x) cout << #x << " " << x << endl;
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+
+#include<stack>
+
+int* stockSpan(int *price, int size) {
+	// Write your code here
+    stack<int> s;
+    s.push(0);
+    int *output = new int[size];
+    output[0] = 1;
+    for (int i = 1; i < size; i++){
+        while(!s.empty() && price[s.top()] < price[i]) s.pop();
+        if (s.empty()) output[i] = 1 + i;
+        else output[i] = i-s.top();
+        s.push(i);
+    }
+    s.pop();
+    return output;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+   //  freopen("in" , "r" , stdin);
+   //  freopen("out" , "w" , stdout);
+    #endif
+
+    return 0;
+ }
